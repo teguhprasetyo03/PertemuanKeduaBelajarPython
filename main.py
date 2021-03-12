35ed458cb9c819e318f899789536624d6456c984
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-03-12 13:48:21 +0700

    commit bro kaya hubungan

---
+++ main.py
@@ -0,0 +1,54 @@
+# tipe data
+# adalah cara kita memberitahu komputer untuk mengelompokkan
+# data berdasarkan apa yang di pahami oleh komputer
+
+a = 25.7 # float -> bilangan desimal
+b = 20 # integer -> bilangan bulat
+d = "Belajar Python" # string -> huruf/karakter
+e = 'Belajar Python untuk pemula' # -> Single Quotes
+j = 4j # tipe data complex
+true = True # tipe data boolean = true, false
+list =["Satu, Dua, Tiga"]
+
+k = 3
+angka = (50 - 5*6) / 4
+nilai = 17//3
+print(list)
+
+# Operator Pada Python
+# Perkalian ( * )
+# Pembagian ( / )
+# Penjumlahan ( + )
+# Pengurangan ( - )
+# Division -> Sisa Hasil Bagi ( % ) / Modulus
+angka1 = 10
+angka2 = 3
+c = b*a  # -> perkalian
+g = b+a # -> penjumlahan
+f = b-a # -> pengurangan
+h = b/a # -> pembagian
+print(angka1 % angka2)
+print("angka \n")
+print(nilai)
+print(d)
+print(c)
+print(g)
+print(f)
+print(h)
+# contenate -> penggabungan
+print("py" + 'thon')
+myscore = 1000
+message = 'I scored %s points'
+print(message % myscore)
+
+# pangkat
+print(5 ** 2)
+
+# tipe data list
+        # 0,1,2,3,4
+square = [1,3,5,7,9] # list index dimulai dari 0
+print(square + [2,4,6,8,10])
+
+listing = [22.5 , 27 , "Teguh", True]
+print(listing[1:4])
+
